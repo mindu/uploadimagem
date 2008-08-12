376b12ad0dc2e6931266077ee61e6002c24810aa
unknown <jmindu@gmail.com>
2008-08-11 21:41:29 -0300

    adicionado o plugin (will_paginate) para paginação de imagens
    Signed-off-by: mindu <jmindu@gmail.com>

--- app/controllers/imagems_controller.rb
+++ app/controllers/imagems_controller.rb
@@ -2,7 +2,8 @@ class ImagemsController < ApplicationController
   # GET /imagems
   # GET /imagems.xml
   def index
-    @imagems = Imagem.find(:all)
+    #@imagems = Imagem.find(:all)
+    @imagems = Imagem.paginate :page => params[:page], :per_page => 2
 
     respond_to do |format|
       format.html # index.html.erb
@@ -13,6 +14,7 @@ class ImagemsController < ApplicationController
   # GET /imagems/1
   # GET /imagems/1.xml
   def show
+  	puts '>>>> ' + params[:page].to_s
     @imagem = Imagem.find(params[:id])
 
     respond_to do |format|
--- app/models/imagem.rb
+++ app/models/imagem.rb
@@ -1,7 +1,7 @@
 class Imagem < ActiveRecord::Base
-	require 'paperclip' 
+ require 'paperclip' 
  has_attached_file :avatar, 
                     :styles => { :medium => "300x300>",
-                                 :thumb => "100x100>" }
+                                 :thumb => "50x50>" }
 
 end

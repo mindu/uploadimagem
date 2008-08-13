5862f288eb2627f0ad1f7028d4f8c43299dded1c
unknown <jmindu@gmail.com>
2008-08-12 21:55:25 -0300

    ajustes na paginação
    Signed-off-by: mindu <jmindu@gmail.com>

--- app/controllers/imagems_controller.rb
+++ app/controllers/imagems_controller.rb
@@ -4,7 +4,7 @@ class ImagemsController < ApplicationController
   def index
     #@imagems = Imagem.find(:all)
     @imagems = Imagem.paginate :page => params[:page], :per_page => 2
-
+    session[:page] = params[:page]
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @imagems }
@@ -14,9 +14,7 @@ class ImagemsController < ApplicationController
   # GET /imagems/1
   # GET /imagems/1.xml
   def show
-  	puts '>>>> ' + params[:page].to_s
     @imagem = Imagem.find(params[:id])
-
     respond_to do |format|
       format.html # show.html.erb
       format.xml  { render :xml => @imagem }
